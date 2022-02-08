24793c0f01ac0d52665008833700bf63f8a9acf1
sagarkivale101 <sagarkivale101@gmail.com>
2022-02-08 12:12:52 +0530

    2nd commit

--- test.sql
+++ test.sql
@@ -1,7 +1,7 @@
 CREATE TABLE studentData(Roll_no INTEGER,Name VARCHAR(30),Address VARCHAR(50),Mobile_Number varchar(50),PAN_Number VARCHAR(20));
 
 
-INSERT INTO studentData VALUES(1, 'sagar', 'pune', '9850399142', 'EIND0555B');
+INSERT INTO studentData VALUES(1, 'sagar', 'pune', '9850399142', 'BAKSDSDAS');
 INSERT INTO studentData VALUES(2, 'ram', 'mumbai', '1238938289', 'JHS67KHJ7');
 INSERT INTO studentData VALUES(3, 'sham', 'delhi', '2340180934', 'DF765849B');
 
